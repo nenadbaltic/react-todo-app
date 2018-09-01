276658658aaf73f66d71da2e15d463a44236c3c4
Nenad <baltic.nenad@gmail.com>
2018-09-02 00:41:34 +0200

    first

---
+++ src/components/Action.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Action = (props) => (
+        <div>
+            <button className="big-button" onClick={props.handlePick}  disabled={props.isDisabled}>What should I do?</button>
+        </div>
+    );
+
+export default Action;
